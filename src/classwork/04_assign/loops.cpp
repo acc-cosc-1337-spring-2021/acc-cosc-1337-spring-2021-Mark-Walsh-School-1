cd3e9699f87f782ce6be32085f1630455a960b6b
Mark-Walsh-School <mark.walsh@g.austincc.edu>
2021-03-03 20:31:00 -0500

    Homework 4

--- src/classwork/04_assign/loops.cpp
+++ src/classwork/04_assign/loops.cpp
@@ -1,1 +1,16 @@
 //cpp
+#include <iostream>
+using namespace std;
+int factorial(int num)
+
+{
+
+    int sentinel = num;
+    for (int i = 1; i < sentinel; i++)
+    {
+        
+        num *= i;
+    }
+    
+    return num;
+}
--- src/classwork/04_assign/main.cpp
+++ src/classwork/04_assign/main.cpp
@@ -1,7 +1,8 @@
 //write includes statements
-
+#include <iostream>;
+#include "loops.h";
 //write using statements for cin and cout
-
+using namespace std;
 
 /*
 Use a do while loop to prompt the user for 
@@ -10,6 +11,38 @@ factorial.  Also, loop continues as long as user wants to.
 */
 int main() 
 {
+
+	string run = "y";
+	int num;
+	string numInput;
+
+	do
+	{
+		bool validInput = false;
+		cout << "\nEnter a whole number between 1 and 10" << endl;
+		
+		while(validInput == false)
+		{
+
+			cin >> num;
+
+			if(num >= 1 && num <= 10)
+			{
+				validInput = true;
+			}
+			else
+			{
+				cout << "\nInvalid input, please enter a whole number between 1 and 10" << endl;
+			}
+
+		}
+
+		cout << "\nThe factorial of " << num << " is " << factorial(num) << endl;
+
+		cout << "Enter 'y' to continue ";
+		cin >> run;
+	} while (run == "y" || run == "Y");
+	
 
 	return 0;
 }
